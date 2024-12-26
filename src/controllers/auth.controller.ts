6938a0fcb1d0104826b254257e7eb3f29d3b19e5
Sarthak Kumar Patro <patrosarthak3@gmail.com>
2024-12-26 22:12:08 +0530

    all API's verified via postman, authroization required for PUT,POST & DELETE calls

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,35 +5,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 // Login Controller
-// export const loginUser = asyncHandler(async (req: Request, res: Response) => {
-//   const { username, password } = req.body;
 
-//   // Check if the user exists
-//   const user = await UserModel.findOne({ username });
-//   if (!user) {
-//     res.status(401);
-//     throw new Error("Invalid credentials");
-//   }
-
-//   // Check if the password is correct
-//   const isMatch = await bcrypt.compare(password, user.password);
-//   if (!isMatch) {
-//     res.status(401);
-//     throw new Error("Invalid credentials");
-//   }
-
-//   // Generate a JWT
-//   const token = jwt.sign(
-//     { userId: user._id, role: user.role },
-//     process.env.JWT_SECRET || "secret",
-//     {
-//       expiresIn: "1h", // Token expiration time
-//     }
-//   );
-
-//   // Send the token in the response
-//   res.json({ token });
-// });
 export const loginUser = asyncHandler(async (req: Request, res: Response) => {
   const { username, password } = req.body;
 
--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -1,19 +1,19 @@
-import express from 'express';
-import { 
-  getAllMovies, 
-  searchMovies, 
-  addMovie, 
-  updateMovie, 
-  deleteMovie 
-} from '../controllers/movie.controller';
-import { auth, adminAuth } from '../middleware/auth.middleware';
+import express from "express";
+import {
+  getAllMovies,
+  searchMovies,
+  addMovie,
+  updateMovie,
+  deleteMovie,
+} from "../controllers/movie.controller";
+import { auth, adminAuth } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
-router.get('/movies', getAllMovies);
-router.get('/search', searchMovies);
-router.post('/movies', [auth, adminAuth], addMovie);
-router.put('/movies/:id', [auth, adminAuth], updateMovie);
-router.delete('/movies/:id', [auth, adminAuth], deleteMovie);
+router.get("/movies", getAllMovies);
+router.get("/search", searchMovies);
+router.post("/movies", [auth, adminAuth], addMovie);
+router.put("/movies/:id", [auth, adminAuth], updateMovie);
+router.delete("/movies/:id", [auth, adminAuth], deleteMovie);
 
 export default router;
